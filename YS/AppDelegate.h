6c4f263aef59a453dc235aab18161ca0618768b4
刘金壮 <liujinzhuang@liujinzhuangdemacbook-pro.local>
2017-05-16 14:16:58 +0800

    Initial Commit

---
+++ YS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YS
+//
+//  Created by 刘金壮 on 17/5/16.
+//  Copyright © 2017年 北京山水风尚科技发展有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YS
+//
+//  Created by 刘金壮 on 17/5/16.
+//  Copyright © 2017年 北京山水风尚科技发展有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
